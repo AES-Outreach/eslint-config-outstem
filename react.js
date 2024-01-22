2499f5c5c831c5be8486786ddedb87afdc87a976
Reid Tull <reid.a.tull@gmail.com>
2024-01-22 09:27:28 -0500

    added extensions and react rules

--- react.js
+++ react.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ['./base.js', './naming/react', './overrides.js'],
+  extends: [
+    './base.js', 
+    './naming/react', 
+    './overrides.js',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 };
